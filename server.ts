8681cf47ff9bde37d175642601ee5b117d78d215
Lehdev <lars@lehdev.de>
2023-12-08 15:39:17 +0100

    feat: add build script

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
 import app from "./src/index"
 import load_dotenv from "dotenv"
+import rateLimit from "express-rate-limit"
 
 load_dotenv.config()
 
 const port = process.env.PORT || '3000'
 
+// Rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/`)
-    }
-)
+    console.log(`Server listening on port ${port}`)
+});
